7d1a9c59b80e39e850f09ff772ba71d2cb069173
Krystan HuffMenne <krystan@tilde.io>
2022-02-16 15:55:52 -0800

    Setup ember-cli-deploy

--- docs/config/environment.js
+++ docs/config/environment.js
@@ -46,7 +46,8 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    ENV.rootURL = '/event-perk-grid';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
