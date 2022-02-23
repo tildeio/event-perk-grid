c3549268cc7a6782900954f5704dc3c7abcbf303
Krystan HuffMenne <krystan@tilde.io>
2022-02-22 22:07:22 -0800

    Add docs for using the APIs

--- docs/app/components/examples/apis/index.js
+++ docs/app/components/examples/apis/index.js
@@ -3,7 +3,9 @@ import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency';
 import { modifier } from 'ember-modifier';
 import ENV from 'docs/config/environment';
+
 import { fetchData, render } from 'event-perk-grid';
+
 // Optionally import this file for basic CSS
 import 'event-perk-grid/index.css';
 
