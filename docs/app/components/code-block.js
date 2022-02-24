d5e244f4a7d254b062f31744a5192bf160477f46
Krystan HuffMenne <krystan@tilde.io>
2022-02-23 16:17:38 -0800

    Add syntax highlighting

--- docs/app/components/code-block.js
+++ docs/app/components/code-block.js
@@ -1,5 +1,7 @@
+import { htmlSafe } from '@ember/template';
 import Component from '@glimmer/component';
 import { task, timeout } from 'ember-concurrency';
+import hljs from 'highlight.js';
 
 export default class CodeBlock extends Component {
   @task({ restartable: true }) *copyText(event) {
@@ -7,5 +9,11 @@ export default class CodeBlock extends Component {
     navigator.clipboard.writeText(this.args.text);
 
     yield timeout(2000);
+  }
+
+  get highlightedText() {
+    return htmlSafe(
+      hljs.highlight(this.args.text, { language: this.args.language }).value
+    );
   }
 }
